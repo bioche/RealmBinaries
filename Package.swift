837f2da03c45ba47aaa20dda4f4e66073d8f4a4f
Tim Studt <timstudt@users.noreply.github.com>
2022-08-19 20:38:41 +0200

    Update Package.swift
    10.28.6

--- Package.swift
+++ Package.swift
@@ -3,30 +3,23 @@
 
 import PackageDescription
 
-let realmVersion = "10.26.0"
+let realmVersion = "10.28.6"
 
 func buildTargets() -> [Target] {
     let baseURL = "https://github.com/timstudt/RealmBinaries/releases/download/\(realmVersion)"
     
-#if swift(>=5.6.0)
-    let xcodeVersion = "13_4"
-    let realmChecksum = "9f14ea8bb48465a475713d4ea8aafc3f65534915e45ae0f8562b54979fad6fe0"
-    let realmSwiftChecksum = "47df781f94dabafddb18e5d525d0f9bb56cfb5f0aed5ca3796f64e9ebef19d86"
-#else
-    let xcodeVersion = "13_3_1"
-    let realmChecksum = "8a2ea9e27a0cb362e79ea72dfba0e51043f4b94831198a0db6984b6af93918de"
-    let realmSwiftChecksum = "8f3a4652560d562e3ec6c70db9d8c544e096ed3a034a49699b66ea2954b86f8b"
-#endif
+    let realmChecksum = "957c915ef31f71be56d2c8a4e59f5eeb05225a1b176d27af7f25ad2d0adb2996"
+    let realmSwiftChecksum = "d5085065ff67eeb1ccddc332290b95c7180e57e8db9cd791bc2dfb2745fd2f61"
     
     return [
         .binaryTarget(
             name: "Realm",
-            url: "\(baseURL)/Realm_\(xcodeVersion).xcframework.zip",
+            url: "\(baseURL)/Realm.xcframework.zip",
             checksum: realmChecksum
         ),
         .binaryTarget(
             name: "RealmSwift",
-            url: "\(baseURL)/RealmSwift_\(xcodeVersion).xcframework.zip",
+            url: "\(baseURL)/RealmSwift.xcframework.zip",
             checksum: realmSwiftChecksum
         )
     ]
@@ -34,7 +27,12 @@ func buildTargets() -> [Target] {
 
 let package = Package(
     name: "RealmBinaries",
-    platforms: [.iOS("13.4")],
+    platforms: [
+        .macOS(.v10_10),
+        .iOS(.v11),
+        .tvOS(.v9),
+        .watchOS(.v2)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(

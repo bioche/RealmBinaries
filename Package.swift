70465a04d86b643b6bc223ed65699d6bcb244711
Éric Blachère <eric.blachere.n7@gmail.com>
2023-01-30 12:44:29 +0100

    [TECH] Realm 10.34.1 + update readme

--- Package.swift
+++ Package.swift
@@ -3,23 +3,19 @@
 
 import PackageDescription
 
-let realmVersion = "10.30.0-beta2"
+let realmVersion = "10.34.1"
 
 func buildTargets() -> [Target] {
     let baseURL = "https://github.com/bioche/RealmBinaries/releases/download/\(realmVersion)"
     
-#if swift(>=5.7.1)
-    let xcodeVersion = "14_1"
-    let realmChecksum = "91663b8e0658a5d5af353d1c6fdc37f37d540d4ebbc0f36f947368809801a000"
-    let realmSwiftChecksum = "d441071be1f716ceb8c1551bb18d6534e36bdc39cf122482bea607f2c84a578a"
-#elseif swift(>=5.7.0)
-    let xcodeVersion = "14_0"
-    let realmChecksum = "e41b992812da0f22fe52d813fa235c21cc39e9d028b98b24fb234035f0a31b8e"
-    let realmSwiftChecksum = "81fe3250331c10300a56c27803fe71c7545b982953a764ffa393160954948c20"
+#if swift(>=5.7.2)
+    let xcodeVersion = "14_2"
+    let realmChecksum = "ce04b1536fea18d72d5a3bc1c6ea5838aae48bb74717f7b3c6b54fa0d332c182"
+    let realmSwiftChecksum = "df5b0925a02a785b14685413b3cfb61336aefa92f396a8bc8622254a0b7579f8"
 #else
     let xcodeVersion = "13_4_1"
-    let realmChecksum = "b343d3bc78d0270ee5f615ab3ae6cdde1f835e216f38c68885dd78ddeb1e76e4"
-    let realmSwiftChecksum = "77fc7f8c459240b3b777feb75cee0bec33db7e504d6f2caa69918a1bc2233a86"
+    let realmChecksum = "d4c2c4f1ca23b6f8699bcc2f771acf75da8ec0c2c1f3851df68d7f37fbb6142f"
+    let realmSwiftChecksum = "986bc9988dbef675955701d58487363edae92e59ede9a69fc76fa6c5d90401d1"
 #endif
     
     return [
--- README.md
+++ README.md
@@ -6,5 +6,9 @@ How to release a new version :
 - It should contain multiple xcode versions of Realm & RealmSwift xcframeworks
 - Compress & rename the last 2 versions of these frameworks (ex : "Realm_13_4_1.xcframework.zip", "RealmSwift_13_4_1.xcframework.zip", "Realm_14_0.xcframework.zip", "RealmSwift_14_0.xcframework.zip")
 - Run swift package compute-checksum <path> from this repo folder for each of the 4 zips
-- Replace version & checksums in package.swift
+- In Package.swift :
+    - Update `realmVersion` variable value to new Realm version (ex: "10.34.1")
+    - Replace swift version with version from current XCode (found here https://xcodereleases.com/)
+    - Replace xcode version & checksums
+    - Update iOS/macOS/watchOS/tvOS handled versions if Realm handled versions changed 
 - Push, tag with the same version number as original repo & upload all 4 zips as assets of the release
